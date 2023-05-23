5cf59eb8155785611cdb1e1f210c824082e88d4f
COQUARD Cyrille <coquardcyr@gmail.com>
2023-05-23 23:46:17 +0200

    Added unit test for events

--- tests/Fixtures/inc/Engine/Cron/Subscriber/getSubscribedEvents.php
+++ tests/Fixtures/inc/Engine/Cron/Subscriber/getSubscribedEvents.php
@@ -1,11 +1,14 @@
 <?php
 return [
-    '' => [
+    'shouldRegister' => [
         'config' => [
 
         ],
         'expected' => [
-
+	        'cron_schedules'                         => 'add_interval',
+	        'init'                                   => 'schedule_cron',
+	        "prefixprocess_scheduled_posts" => 'process_scheduled_posts',
+	        "prefixprocess_scheduled" => ['process_scheduled', 10, 3],
         ]
     ],
 
